1ab13ac5560155342dac0da426f15e991f5615f6
Zarpyk <30746531+Zarpyk@users.noreply.github.com>
2025-03-18 06:53:59 +0100

    Enhance Vector2 class with equals, hashCode, and toString methods

--- model/src/main/java/com/hexadeventure/model/map/Vector2.java
+++ model/src/main/java/com/hexadeventure/model/map/Vector2.java
@@ -1,11 +1,27 @@
 package com.hexadeventure.model.map;
 
+import lombok.AllArgsConstructor;
+
+import java.util.Objects;
+
+@AllArgsConstructor
 public class Vector2 {
     public int x;
     public int y;
     
-    public Vector2(int x, int y) {
-        this.x = x;
-        this.y = y;
+    @Override
+    public boolean equals(Object o) {
+        if(!(o instanceof Vector2 vector2)) return false;
+        return x == vector2.x && y == vector2.y;
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+    
+    @Override
+    public String toString() {
+        return '(' + x + ", " + y + ')';
     }
 }
