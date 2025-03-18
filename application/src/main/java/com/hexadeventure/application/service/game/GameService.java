b4a77798aa711ed13fe40dda486df696ef02308d
Zarpyk <30746531+Zarpyk@users.noreply.github.com>
2025-03-18 06:33:04 +0100

    Refactor map generation logic into separate methods and generate character on it.

--- application/src/main/java/com/hexadeventure/application/service/game/GameService.java
+++ application/src/main/java/com/hexadeventure/application/service/game/GameService.java
@@ -6,6 +6,7 @@ import com.hexadeventure.application.port.out.noise.NoiseGenerator;
 import com.hexadeventure.application.port.out.persistence.GameMapRepository;
 import com.hexadeventure.application.port.out.persistence.UserRepository;
 import com.hexadeventure.model.map.GameMap;
+import com.hexadeventure.model.map.Vector2;
 import com.hexadeventure.model.user.User;
 
 import java.util.Optional;
@@ -28,19 +29,35 @@ public class GameService implements GameUseCase {
         assert user.isPresent();
         Optional<GameMap> map = gameMapRepository.findById(user.get().getMapId());
         if(map.isEmpty()) {
-            GameMap newMap = new GameMap(email, seed, size);
-            noiseGenerator.initNoise(email, seed);
-            for (int x = 0; x < size; x++) {
-                for (int y = 0; y < size; y++) {
-                    double noise = noiseGenerator.getPerlinNoise(x, y, email);
-                    newMap.createCell(noise, x, y);
-                }
-            }
-            noiseGenerator.releaseNoise(email);
+            GameMap newMap = generateMap(email, seed, size);
             gameMapRepository.save(newMap);
             userRepository.updateMapIdByEmail(email, newMap.getId());
         } else {
             throw new GameStartedException();
         }
+    }
+    
+    private GameMap generateMap(String email, long seed, int size) {
+        GameMap map = new GameMap(email, seed, size);
+        generateCells(email, seed, size, map);
+        generatePlayer(map);
+        return map;
+    }
+    
+    private void generateCells(String email, long seed, int size, GameMap map) {
+        noiseGenerator.initNoise(email, seed);
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                double noise = noiseGenerator.getPerlinNoise(x, y, email);
+                map.createCell(noise, x, y);
+            }
+        }
+        noiseGenerator.releaseNoise(email);
+    }
+    
+    private void generatePlayer(GameMap map) {
+        int mapSize = map.getMapSize();
+        Vector2 center = new Vector2(mapSize / 2, mapSize / 2);
+        map.initMainCharacter(center);
     }
 }
