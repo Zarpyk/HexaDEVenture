236c2f2406d9138361df21f279562aaf25803339
Zarpyk <30746531+Zarpyk@users.noreply.github.com>
2025-03-21 03:22:49 +0100

    Add JNoiseAdapterTest

---
+++ adapter/src/test/java/com/hexadeventure/adapter/out/noise/JNoiseAdapterTest.java
@@ -0,0 +1,42 @@
+package com.hexadeventure.adapter.out.noise;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+public class JNoiseAdapterTest {
+    private final JNoiseAdapter noiseAdapter;
+    
+    public JNoiseAdapterTest() {
+        noiseAdapter = new JNoiseAdapter();
+    }
+    
+    @BeforeEach
+    public void beforeEach() {
+        noiseAdapter.initNoise("test", 1, 0.1,
+                               4, 0.2, 1.5, 0,
+                               true, true);
+    }
+    
+    @AfterEach
+    public void afterEach() {
+        noiseAdapter.releaseNoise("test");
+    }
+    
+    @Test
+    public void whenGenerateNonNormalizedNoise_thenNoiseIsGenerated() {
+        double noise = noiseAdapter.getPerlinNoise(0, 0, "test", false);
+        assertThat(noise).isBetween(-1.0, 1.0);
+    }
+    
+    @Test
+    public void whenGenerateNormalizedNoise_thenNoiseIsGenerated() {
+        double noise = noiseAdapter.getPerlinNoise(0, 0, "test", true);
+        assertThat(noise).isBetween(0.0, 1.0);
+    }
+}
