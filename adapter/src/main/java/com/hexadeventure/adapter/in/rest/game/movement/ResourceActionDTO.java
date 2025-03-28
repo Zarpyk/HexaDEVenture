656432827413a4d70651eb5a3c259f210e1ee8eb
Zarpyk <30746531+Zarpyk@users.noreply.github.com>
2025-03-28 17:33:31 +0100

    Fix type

--- adapter/src/main/java/com/hexadeventure/adapter/in/rest/game/movement/ResourceActionDTO.java
+++ adapter/src/main/java/com/hexadeventure/adapter/in/rest/game/movement/ResourceActionDTO.java
@@ -2,7 +2,7 @@ package com.hexadeventure.adapter.in.rest.game.movement;
 
 import com.hexadeventure.model.movement.ResourceAction;
 
-public record ResourceActionDTO(String type, int amount) {
+public record ResourceActionDTO(int type, int amount) {
     public static ResourceActionDTO fromModel(ResourceAction resourceAction) {
         return new ResourceActionDTO(resourceAction.type(), resourceAction.amount());
     }
