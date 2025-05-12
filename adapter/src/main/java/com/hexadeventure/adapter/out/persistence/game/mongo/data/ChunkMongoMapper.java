398ace2149c3aca990641a8ba6fac6392a64ef17
Zarpyk <30746531+Zarpyk@users.noreply.github.com>
2025-05-12 23:52:51 +0200

    Allow Loot array to serialize

--- adapter/src/main/java/com/hexadeventure/adapter/out/persistence/game/mongo/data/ChunkMongoMapper.java
+++ adapter/src/main/java/com/hexadeventure/adapter/out/persistence/game/mongo/data/ChunkMongoMapper.java
@@ -12,6 +12,7 @@ import com.hexadeventure.adapter.out.persistence.game.mongo.ChunkMongoSDReposito
 import com.hexadeventure.adapter.utils.Vector2CDeserializer;
 import com.hexadeventure.adapter.utils.Vector2Deserializer;
 import com.hexadeventure.model.enemies.Enemy;
+import com.hexadeventure.model.inventory.characters.Loot;
 import com.hexadeventure.model.inventory.characters.PlayableCharacter;
 import com.hexadeventure.model.map.CellData;
 import com.hexadeventure.model.map.Chunk;
@@ -33,6 +34,7 @@ public class ChunkMongoMapper {
                                                                     .allowIfBaseType("com.hexadeventure")
                                                                     .allowIfSubType(PlayableCharacter[][].class)
                                                                     .allowIfSubType(PlayableCharacter[].class)
+                                                                    .allowIfSubType(Loot[].class)
                                                                     .allowIfSubType(CellData[][].class)
                                                                     .allowIfSubType(CellData[].class)
                                                                     .allowIfSubType(HashMap.class)
