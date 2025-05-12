3baae25c16a91ddce09d97f981b13f484199b2b7
Zarpyk <30746531+Zarpyk@users.noreply.github.com>
2025-05-12 23:49:30 +0200

    Add @NoArgsConstructor for json serialization

--- model/src/main/java/com/hexadeventure/model/map/resources/Resource.java
+++ model/src/main/java/com/hexadeventure/model/map/resources/Resource.java
@@ -3,17 +3,19 @@ package com.hexadeventure.model.map.resources;
 import com.hexadeventure.model.map.Vector2;
 import com.hexadeventure.utils.DoubleMapper;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.SplittableRandom;
 
 @Getter
+@NoArgsConstructor
 public class Resource {
     private static final int MIN_COUNT = 1;
     private static final int MAX_COUNT = 6;
     
-    private final Vector2 position;
-    private final ResourceType type;
-    private final int count;
+    private Vector2 position;
+    private ResourceType type;
+    private int count;
     
     public Resource(Vector2 position, double threshold, SplittableRandom random) {
         this.position = position;
